15185172854dea4855b999ba192d7836f51c91d3
Christian Awelakoue <awelakoue@gmail.com>
2022-02-02 06:57:40 +0200

    products section

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { StyledProducts } from "./styles/Products.styled";
+import product1 from "../assets/product1.jpg";
+import product2 from "../assets/product2.jpg";
+import product3 from "../assets/product3.jpg";
+import product4 from "../assets/product4.jpg";
 
 export default function Products() {
+  const data = [
+    {
+      image: product1,
+      name: "Chicken Burger",
+      price: "R122.4",
+    },
+    {
+      image: product2,
+      name: "Toasted Bread",
+      price: "R15.5",
+    },
+    {
+      image: product3,
+      name: "Egg Sandwich",
+      price: "R18",
+    },
+
+    {
+      image: product4,
+      name: "Raspberry Cake",
+      price: "R112.5",
+    },
+  ];
   return (
-    <div>
-      
-    </div>
-  )
+    <StyledProducts>
+      <div className='title'>
+        <h1>
+          <span>Favourite</span> All the time!
+        </h1>
+      </div>
+      <div className='products'>
+        {data.map((product) => {
+          return (
+            <div className='product'>
+              <div className='image'>
+                <img src={product.image} alt='' />
+              </div>
+              <h2>{product.name}</h2>
+              <h3>{product.price}</h3>
+              <p>He Printing and Typesetting the industry. Lorem Ipsum has</p>
+              <button>Buy Now</button>
+            </div>
+          );
+        })}
+      </div>
+    </StyledProducts>
+  );
 }
--- src/components/styles/Products.styled.jsx
+++ src/components/styles/Products.styled.jsx
@@ -1,5 +1,64 @@
 import styled from "styled-components";
+import { imageZoomEffect, TitleStyles } from "./Reusable.styled";
 
 export const StyledProducts = styled.section`
+  ${TitleStyles};
+  .products {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 3rem;
+    margin-top: 3rem;
+    .product {
+      display: flex;
+      flex-direction: column;
+      gap: 0.6rem;
+      justify-content: center;
+      align-items: center;
+      h3 {
+        color: #fc4958;
+      }
+      p {
+        text-align: center;
+        font-size: 1.1rem;
+        line-height: 2rem;
+        letter-spacing: 0.1rem;
+      }
+      ${imageZoomEffect};
+      .image {
+        max-height: 20rem;
+        overflow: hidden;
+        border-radius: 1rem;
+        img {
+          height: 20rem;
+          width: 15rem;
+          object-fit: cover;
+        }
+      }
+      button {
+        border: none;
+        padding: 1rem 4rem;
+        font-size: 1.4rem;
+        color: white;
+        border-radius: 4rem;
+        transition: 0.5s ease-in-out;
+        cursor: pointer;
+        background: linear-gradient(to right, #fc4958, #e85d04);
+        text-transform: uppercase;
+        &:hover {
+          background: linear-gradient(to right, #e85d04, #fc4958);
+        }
+      }
+    }
+  }
 
+  @media screen and (min-width: 280px) and (max-width: 720px) {
+    .products {
+      grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    }
+  }
+  @media screen and (min-width: 720px) and (max-width: 1080px) {
+    .products {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
 `;
