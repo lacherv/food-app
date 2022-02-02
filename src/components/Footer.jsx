0a59cfc96fe11dacc3cad4a93892be481067a1dc
Christian Awelakoue <awelakoue@gmail.com>
2022-02-02 07:17:34 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import { StyledFooter, LowerFooter } from "./styles/Footer.styled";
+import logo from "../assets/FoodYummy.png";
+import { AiFillInstagram } from "react-icons/ai";
+import { BsTwitter } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+import { GrLinkedinOption } from "react-icons/gr";
 
 export default function Footer() {
   return (
-    <div>
-      
+    <div className='footer'>
+      <StyledFooter>
+        <div className='brand container'>
+          <img src={logo} alt='' />
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis,
+            repudiandae.
+          </p>
+          <ul>
+            <li>
+              <AiFillInstagram />
+            </li>
+            <li>
+              <FaFacebookF />
+            </li>
+            <li>
+              <GrLinkedinOption />
+            </li>
+            <li>
+              <BsTwitter />
+            </li>
+          </ul>
+        </div>
+        <div className='about container'>
+          <div className='title'>
+            <h3>About Us</h3>
+          </div>
+          <p>
+            If you are going to use a passage of Lorem Ipsum, you need to be
+            sure there isn't anything embarrassing hidden in the middle of text
+          </p>
+        </div>
+        <div className='contact container'>
+          <div className='title'>
+            <h3>Contact Us</h3>
+          </div>
+          <p>+27 112 3123 </p>
+          <p>foodyama@example.com</p>
+          <p>@foodyama</p>
+          <p>412 Street, Ground Floor, South Africa</p>
+        </div>
+      </StyledFooter>
+      <LowerFooter className='lower__footer'>
+        <h2>
+          Copyright &copy; 2022 <span>Food Yama</span>
+        </h2>
+      </LowerFooter>
     </div>
-  )
+  );
 }
---
+++ src/components/styles/Footer.styled.jsx
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  margin: 0;
+  background: linear-gradient(to right, #fc4958, #e85d04);
+  color: white;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10vw;
+  padding: 4vw;
+  p {
+    font-size: 1.1rem;
+    line-height: 2rem;
+    letter-spacing: 0.1rem;
+  }
+  ul {
+    display: flex;
+    list-style-type: none;
+    gap: 4vw;
+    margin-top: 2vw;
+    li {
+      padding: 0.8rem;
+      border-radius: 2rem;
+      background-color: white;
+      transition: 0.3s ease-in-out;
+      cursor: pointer;
+      &:hover {
+        background-color: black;
+        svg {
+          transform: scale(1.2);
+        }
+      }
+      svg {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #fc4958;
+        font-size: 1.6rem;
+        transition: 0.3s ease-in-out;
+        &:hover {
+        }
+      }
+    }
+  }
+  img {
+    filter: brightness(0) invert(1);
+    width: 10vw;
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    h3 {
+      font-size: 2rem;
+    }
+  }
+  @media screen and (min-width: 260px) and (max-width: 1080px) {
+    grid-template-columns: 1fr;
+    .container {
+      img {
+        height: 4rem;
+        width: 10rem;
+      }
+    }
+  }
+`;
+
+export const LowerFooter = styled.div`
+  margin: 0;
+  text-align: center;
+  background-color: black;
+  color: white;
+  padding: 1rem;
+  h2 {
+    span {
+      color: #fc4958;
+      text-transform: uppercase;
+    }
+  }
+  @media screen and (min-width: 260px) and (max-width: 450px) {
+    h2 {
+      span {
+        display: block;
+      }
+    }
+  }
+`;
